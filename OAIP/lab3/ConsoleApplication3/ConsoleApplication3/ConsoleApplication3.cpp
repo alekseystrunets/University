ce8c4fea9baa54b3fa13a1c67816027651af9a47
alekseystrunets <145933314+alekseystrunets@users.noreply.github.com>
2024-12-22 15:27:17 +0300

    Add files via upload

---
+++ OAIP/lab3/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+  int main() {
+	double x, a = 0.1, b = 1., h = 0.09, y, s, r, m;
+	int k, n ;
+	cout << "Zadai n="; cin >> n;
+	for (x = a; x <= b; x += h) {
+		r = s = x;
+		for (k = 1; k <= n; k++) //k степень
+		{
+			r = r * x * x / ((2 * k + 1) * 2 * k);
+			s += r;;
+		}
+		y = (exp(x) - exp(-x)) / 2;
+		m = fabs(y - s);
+		cout << "chislo x= " << x << "  chislo y= " << y << " chislo m= " << m << "  chislo s= " << s << endl;
+
+		//	return 0;
+
+
+	}
+
+}
