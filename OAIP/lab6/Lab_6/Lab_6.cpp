f51b023900a237a4d959c17dd568196ddf1e9bf2
alekseystrunets <145933314+alekseystrunets@users.noreply.github.com>
2024-12-22 15:29:14 +0300

    Add files via upload

---
+++ OAIP/lab6/Lab_6/Lab_6.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char str[256];
+
+    cout << "Enter string: ";
+    cin >> str;
+
+    cout << "Numbers in the string: " << endl;
+
+    for (int i = 0; i < strlen(str); i++) 
+        if (isdigit(str[i]))
+            cout << str[i] << " ";
+
+    return 0;
+}
